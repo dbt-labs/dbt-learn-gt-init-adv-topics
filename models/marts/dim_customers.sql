e94d7c1526f2c9d83046f41405f8f093681dc467
Eric Flores <eric.flores2@dbtlabsworkshop.com>
2025-09-22 22:19:33 +0000

    fixing issues

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,7 +1,7 @@
 with customers as (
 
     select
-        id as customer_id,
+        customer_id,
         first_name,
         last_name
 
@@ -12,13 +12,13 @@ with customers as (
 orders as (
 
     select
-        id as order_id,
-        user_id as customer_id,
+        order_id,
+        customer_id,
         order_date,
-        status,
+        order_status,
         _etl_loaded_at
 
-    from raw.jaffle_shop.orders
+    from {{ ref('stg_jaffle_shop__orders') }}
 
 ),
 
