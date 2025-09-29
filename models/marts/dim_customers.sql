7ef8040ed5f8cbc00cd0d96de66a61235719f714
Christian Bautista <christian.bautista@dbtlabsworkshop.com>
2025-09-29 19:16:29 +0000

    model changes

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,24 +1,24 @@
 with customers as (
 
     select
-        id as customer_id,
+        customer_id,
         first_name,
         last_name
 
-    from raw.jaffle_shop.customers
+    from {{ ref('stg_jaffle_shop__customers') }}
 
 ),
 
 orders as (
 
     select
-        id as order_id,
-        user_id as customer_id,
+        order_id,
+        customer_id,
         order_date,
-        status,
+        order_status,
         _etl_loaded_at
 
-    from raw.jaffle_shop.orders
+    from {{ ref('stg_jaffle_shop__orders') }}
 
 ),
 
@@ -26,7 +26,6 @@ customer_orders as (
 
     select
         customer_id,
-
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
         count(order_id) as number_of_orders
