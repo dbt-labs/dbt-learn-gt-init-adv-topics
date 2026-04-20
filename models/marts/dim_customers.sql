9e0e8833efb776ae40067afff3807dabe8cc6490
mark wan <mark.wan+1776232481@dbtlabsworkshop.com>
2026-04-20 05:15:57 +0000

    commit

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -7,23 +7,23 @@
 with customers as (
 
     select
-        id as customer_id,
+        customer_id,
         first_name,
         last_name
 
-    from {{ source('jaffle_shop', 'customers') }} 
+    from {{ ref('stg_jaffle_shop__customers') }}
 
 ),
 
 orders as (
 
     select
-        id as order_id,
-        user_id as customer_id,
+        order_id,
+        customer_id,
         order_date,
         status
 
-    from {{ source('jaffle_shop', 'orders') }} 
+    from {{ ref('stg_jaffle_shop__orders') }}
 
 ),
 
---
+++ models/marts/dim_test_model.sql
@@ -0,0 +1,6 @@
+{{
+    config(
+        materialized='view'
+    )
+}}
+select * from {{ ref('stg_test_model') }}
