3c1800fd9f2d1fa040131951167fbb7e212af6de
david opoku <david.opoku+1779062212@dbtlabsworkshop.com>
2026-05-19 02:01:04 +0000

    Add the orders_by_day.sql file to the analysis folder

---
+++ analysis/orders_by_day.sql
@@ -0,0 +1,11 @@
+with orders as (
+    select order_date, count(*) as orders
+    from {{ ref('stg_jaffle_shop__orders') }}
+    group by order_date
+  ),
+  previous_day_orders as (
+    select order_date, lag(orders) over (order by order_date) as previous_day_orders
+    from orders
+  )
+  select *
+  from previous_day_orders
