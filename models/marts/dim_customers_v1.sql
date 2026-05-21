13d81ce666361bfd938f5a5c3e55ea0f49fb4c5f
stephane bizard <stephane.bizard@dbtlabsworkshop.com>
2026-05-21 14:51:36 +0000

    evol

--- models/marts/dim_customers_v1.sql
+++ models/marts/dim_customers_v1.sql
@@ -25,7 +25,8 @@ final as (
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
         coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
-        customer_orders.lifetime_value
+        customer_orders.lifetime_value as lifetime_value
+
     from customers
     left join customer_orders using (customer_id)
 )
