ac641ef9fca9db18303082c89b7b7a77428da003
stephane bizard <stephane.bizard@dbtlabsworkshop.com>
2026-05-22 08:01:23 +0000

    evols

--- tests/generic/date_greater_than.sql
+++ tests/generic/date_greater_than.sql
@@ -1,8 +1,16 @@
 {% test date_greater_than(model, column_name, date_field) %}
 
 
+
+
+
+{{ config(warn_if = '<50', error_if = '>=50') }}
+
+
    select * from {{ model }}
    where {{ column_name }} < '{{ date_field }}'
 
 
 {% endtest %}
+
+
